0b7941cc63a1989d0fe4ba1b673ce4d2b40a0075
1305096t <1305096t@gmail,com>
2017-04-14 13:40:30 +0900

    w02-task2 commit

--- w02/task1.js
+++
@@ -1,8 +0,0 @@
-function calcV(x, y, z){
-
-    var x=5, y=4, z=8;
-    var v = new vec3(x, y, z);
-    var min = v.min();
-    var mid = v.mid();
-    var max = v.max();
-}
--- w02/vec3.js
+++ w02/vec3.js
@@ -4,7 +4,6 @@ vec3 = function(x, y, z){
   this.y = y;
   this.z = z;
 }
-
 // add method
 vec3.prototype.add = function(v){
   this.x += v.x;
@@ -12,7 +11,6 @@ vec3.prototype.add = function(v){
   this.z += v.z;
   return this;
 }
-
 // sum method
 vec3.prototype.sum = function(){
   return this.x + this.y + this.z;
@@ -29,7 +27,6 @@ vec3.prototype.min = function(){
   else if(this.z <= this.x && this.z <= this.y)
     return this.z;
 }
-
 // mid method
 vec3.prototype.mid = function(){
   if((this.z < this.x && this.x < this.y) || (this.y < this.x && this.x < this.z))
@@ -39,7 +36,6 @@ vec3.prototype.mid = function(){
   else if((this.y < this.z && this.z < this.x) || (this.x < this.z && this.z < this.y))
     return this.z;
 }
-
 // max method
 vec3.prototype.max = function(){
   if(this.y <= this.x && this.z <= this.x)
@@ -48,4 +44,20 @@ vec3.prototype.max = function(){
     return this.y;
   else if(this.x <= this.z && this.y <= this.z)
     return this.z;
+}
+
+///w02-task2
+// calculate the square of the length of a vector
+function vsqrt(v){
+  return v.x*v.x+v.y*v.y+v.z*v.z;
+}
+// calculate the inner product of 2 vectors
+function ip(v0, v1){
+  return v0.x*v1.x+v0.y*v1.y+v0.z*v1.z;
+}
+// caluculate the area of a triangle
+function areaOfTriangle(v0, v1, v2){
+  var v10 = new vec3(v1.x-v0.x, v1.y-v0.y, v1.z-v0.z);
+  var v20 = new vec3(v2.x-v0.x, v2.y-v0.y, v2.z-v0.z);
+  return 0.5*Math.sqrt(vsqrt(v10)*vsqrt(v20)-ip(v10, v20)*ip(v10, v20));
 }
