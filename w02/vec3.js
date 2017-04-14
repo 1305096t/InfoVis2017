e5941518423ccbc01bee4b81c2a986f152f9fbe2
1305096t <1305096t@gmail,com>
2017-04-14 12:33:13 +0900

    w02-task1 commit

--- w02/vec3.js
+++ w02/vec3.js
@@ -17,3 +17,35 @@ vec3.prototype.add = function(v){
 vec3.prototype.sum = function(){
   return this.x + this.y + this.z;
 }
+
+
+///w02-task1
+// min method
+vec3.prototype.min = function(){
+  if(this.x <= this.y && this.x <= this.z)
+    return this.x;
+  else if(this.y <= this.z && this.y <= this.x)
+    return this.y;
+  else if(this.z <= this.x && this.z <= this.y)
+    return this.z;
+}
+
+// mid method
+vec3.prototype.mid = function(){
+  if((this.z < this.x && this.x < this.y) || (this.y < this.x && this.x < this.z))
+    return this.x;
+  else if((this.x < this.y && this.y < this.z) || (this.z < this.y && this.y < this.x))
+    return this.y;
+  else if((this.y < this.z && this.z < this.x) || (this.x < this.z && this.z < this.y))
+    return this.z;
+}
+
+// max method
+vec3.prototype.max = function(){
+  if(this.y <= this.x && this.z <= this.x)
+    return this.x;
+  else if(this.z <= this.y && this.x <= this.y)
+    return this.y;
+  else if(this.x <= this.z && this.y <= this.z)
+    return this.z;
+}
